b3b11da77aed8e621bb93c7aa33f5a2fed09d24b
Sondre.Bolland1 <sondre.bolland@student.uib.no>
2023-01-18 11:50:47 +0100

    Add empty repository

---
+++ src/test/java/INF101/Empty/SimpleTest.java
@@ -0,0 +1,19 @@
+package INF101.Empty;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test template.
+ */
+public class SimpleTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
